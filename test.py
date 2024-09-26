0fbb7f3c5c755abc6ac0ee881e9d5b10adf21534
tech08mag <139458159+Tech08mag@users.noreply.github.com>
2024-09-26 19:03:06 +0200

    some bullshit was nicht tun will, was ich will

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
-import customtkinter, yt_dlp
-from messages import Error, Success, Finished
 from tkinter import filedialog
+import yt_dlp
+from pathlib import Path
+from cleanname import clean_filename
+import os, sys, shutil
 
 LOG_STATES = {
     'debug': False,
@@ -23,7 +25,7 @@ def format_selector(ctx):
     print(f"\033[92m[INFO]:\033[00m Best video picked: {best_video['format_note']}")
 
     # find compatible audio extension
-    audio_ext = {'mp4': 'm4a', 'webm': 'webm'}.get(best_video['ext'], None)
+    audio_ext = {'mp4': 'm4a'}.get(best_video['ext'], None)
 
     if audio_ext is None:
         print('\033[91m[ERROR]:\033[00m No compatible audio extension found!')
@@ -41,7 +43,6 @@ def format_selector(ctx):
         # Must be + separated list of protocols
         'protocol': f'{best_video["protocol"]}+{best_audio["protocol"]}'
     }
-
 def progress_hook(d):
     """ Custom hook to print download progress """
     # see help(yt_dlp.YoutubeDL) for all available fields and more information
@@ -116,58 +117,27 @@ class Logger:
         if self.log_states['error']:
             print(f"\033[91m[ERROR]:\033[00m {msg}")
 
-class App(customtkinter.CTk):
-    def __init__(self):
-        super().__init__()
-
-        self.title("Youtube downloader")
-        self.geometry("500x400")
-        self.resizable(True, True)
-        self.toplevel_window = None
-
-        self.main_frame = customtkinter.CTkFrame(self)
-        self.main_frame.pack(padx=10, pady=10, fill='x', expand=True)
-
-        self.entry = customtkinter.CTkEntry(self.main_frame, placeholder_text="Youtube link", fg_color="transparent")
-        self.entry.pack(padx=10, pady=10, fill='x', expand=True)
-
-        self.button = customtkinter.CTkButton(self.main_frame, text="download", command=self.button_callback)
-        self.button.pack(padx=10, pady=10, fill='x', expand=True)
-
-    def open_Error(self):
-            if self.toplevel_window is None or not self.toplevel_window.winfo_exists():
-                self.toplevel_window = Error(self)  # create window if its None or destroyed
-            else:
-                self.toplevel_window.focus()
-
-    def open_Sucess(self):
-            if self.toplevel_window is None or not self.toplevel_window.winfo_exists():
-                self.toplevel_window = Success(self)  # create window if its None or destroyed
-            else:
-                self.toplevel_window.focus()
-    
-    def open_Finished(self):
-        if self.toplevel_window is None or not self.toplevel_window.winfo_exists():
-            self.toplevel_window = Finished(self)  # create window if its None or destroyed
-        else:
-            self.toplevel_window.focus()
-
-    def button_callback(self):
-        URL = self.entry.get()
-        if str(URL) == "":
-            self.open_Error()
-            pass
-        else:
-            self.open_Sucess()
-            ydl_opts = {
+ydl_opts = {
                 'format': format_selector,
                 'progress_hooks': [progress_hook],
                 'logger': Logger(LOG_STATES),
             }
 
-            with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-                ydl.download(URL)
-            self.open_Finished()
+with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+    #scheiß link https://www.youtube.com/watch?v=RCk4b9X8LYI
+    info_dict = ydl.extract_info("https://www.youtube.com/watch?v=VoqJspTMDRM", download=True)
+    
+    video_url = info_dict.get("url", None)
+    video_id = info_dict.get("id", None)
+    video_title = info_dict.get('title', None)
+    source = f"{Path.cwd()}\\{video_title}"
+    newname = f"{Path.cwd()}\\{clean_filename(video_title)}"
+    video_title = os.rename(source, newname)
+    print(os.path.exists(f"{Path.cwd()}"))
+    print(os.path.isfile(f"{video_title}"))
 
-app = App()
-app.mainloop()
+    if os.path.exists(f"{Path.cwd()}") and os.path.isfile(f"{video_title}"):
+        save_path = filedialog.asksaveasfilename(title="Save", filetypes=[("Mp4 Files", ".mp4")], defaultextension=".mp4",
+            initialdir=Path(sys.executable), initialfile=video_title)
+        shutil.move(video_title, save_path)
+
